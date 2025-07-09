858e67a988635b45381e119e55a5d3b06d00e16f
César Guerra <warderer@gmail.com>
2025-07-08 21:00:44 -0600

    💄 Add basic template

--- 01-react-props/src/main.jsx
+++ 01-react-props/src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App saludo="Hola" title="G41A, soy un Prop"/>
-    <App saludo="Hi" title="This is another component with different info"/>
+    <App />
   </StrictMode>,
 )
