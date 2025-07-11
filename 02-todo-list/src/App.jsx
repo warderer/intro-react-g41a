fa809986caf716e4351ae3f2560ce291199d6158
César Guerra <warderer@gmail.com>
2025-07-10 21:02:02 -0600

    ✨ Add handleAdd, deleteTodo and show dynamic tasks

--- 02-todo-list/src/App.jsx
+++ 02-todo-list/src/App.jsx
@@ -1,14 +1,27 @@
 /* 1. Importar el hook useState */
 import { useState } from 'react'
+import ToDoItem from './components/ToDoItem'
 import './App.css'
 
 function App () {
   /* 2. Creo el estado donde guardare la información del input */
   const [inputValue, setInputValue] = useState('')
 
+  /* 5. Crear un nuevo estado para manejar la lista de tareas */
+  const [tasks, setTasks] = useState([])
+
   /* 3. Crear una función que se ejecutará cuando haga clic en añadir */
+  /* 6. Modificar la función para agregar tareas al estado tasks */
   const handleAdd = () => {
-    console.log('Añadir tarea:', inputValue)
+    // console.log('Añadir tarea:', inputValue)
+    setTasks([...tasks, inputValue])
+    setInputValue('') // Limpiar el input después de añadir la tarea
+  }
+
+  /* 7. Crear función para borrar una tarea */
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index)
+    setTasks(newTasks)
   }
 
   return (
@@ -24,9 +37,15 @@ function App () {
       <button onClick={handleAdd}>Añadir</button>
       {/* ul>li{Item $}*3>button{Borrar} */}
       <ul>
-        <li>Item 1<button>Borrar</button></li>
-        <li>Item 2<button>Borrar</button></li>
-        <li>Item 3<button>Borrar</button></li>
+        {
+          tasks.map((task, index) => (
+            <ToDoItem
+              key={index}
+              todoName={task}
+              handleDelete={() => deleteTask(index)}
+            />
+          ))
+        }
       </ul>
     </>
   )
