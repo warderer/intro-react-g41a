848dc3fd730c1abc1a6fb09e37f2c96f8603f6a1
César Guerra <warderer@gmail.com>
2025-07-10 20:29:39 -0600

    ✨ Add State and Add Button

--- 02-todo-list/src/App.jsx
+++ 02-todo-list/src/App.jsx
@@ -1,10 +1,27 @@
+/* 1. Importar el hook useState */
+import { useState } from 'react'
 import './App.css'
+
 function App () {
+  /* 2. Creo el estado donde guardare la información del input */
+  const [inputValue, setInputValue] = useState('')
+
+  /* 3. Crear una función que se ejecutará cuando haga clic en añadir */
+  const handleAdd = () => {
+    console.log('Añadir tarea:', inputValue)
+  }
+
   return (
     <>
       <h1>Todo List</h1>
-      <input type='text' placeholder='Escribe una tarea' />
-      <button>Añadir</button>
+      {/* 4. Implementar el evento onChange para guardar la información del estado, apenas esta sea tecleada */}
+      <input
+        type='text'
+        placeholder='Escribe una tarea'
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <button onClick={handleAdd}>Añadir</button>
       {/* ul>li{Item $}*3>button{Borrar} */}
       <ul>
         <li>Item 1<button>Borrar</button></li>
