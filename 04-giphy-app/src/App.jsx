e480548f3344e8dafd2ee541dae25d753e8051a3
César Guerra <warderer@gmail.com>
2025-07-17 20:13:09 -0600

    ✨ Add Search feature to App

--- 04-giphy-app/src/App.jsx
+++ 04-giphy-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import ImageCard from './components/ImageCard'
 import './App.css'
+import SearchBar from './components/SearchBar'
 
 function App () {
   const [gifs, setGifs] = useState([])
@@ -20,9 +21,20 @@ function App () {
     fetchGifs()
   }, [])
 
+  const sendSearch = async (searchWord) => {
+    try {
+      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${searchWord}&limit=12&offset=0&rating=g&lang=es&bundle=messaging_non_clips`)
+      const results = await response.json()
+      setGifs(results.data)
+    } catch (error) {
+      console.error('Error fetching GIFs:', error)
+    }
+  }
+
   return (
     <>
       <div className='App'>
+        <SearchBar handleSearch={sendSearch} />
         <div className='grid-cards'>
           {gifs.map(gif => (
             <ImageCard
