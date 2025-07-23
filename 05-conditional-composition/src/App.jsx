0c05668760749fa6fecd7db1d7c48fa8133157e3
César Guerra <warderer@gmail.com>
2025-07-22 19:51:46 -0600

    ✨ Add login/logout condicional rendering

--- 05-conditional-composition/src/App.jsx
+++ 05-conditional-composition/src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 
 function App () {
+  // Estado para saber si el usuario ha iniciado sesión o no
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  // Funciones para simular el inicio y cierre de sesión
+  const handleLogin = () => setIsLoggedIn(true)
+
+  const handleLogout = () => setIsLoggedIn(false)
+
   return (
     <div className='container'>
       <h1>Mi Panel de Control</h1>
+
+      {/* --- RENDERIZADO CONDICIONAL CON OP. TERNARIO --- */}
+      {
+        isLoggedIn
+          ? <button onClick={handleLogout}>Cerrar sesión</button>
+          : <button onClick={handleLogin}>Iniciar sesión</button>
+      }
+
     </div>
   )
 }
