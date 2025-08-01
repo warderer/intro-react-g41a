ca62a8f8c2a04e41a8d71d1fd9efeb7d6964fc0d
César Guerra <warderer@gmail.com>
2025-07-31 19:35:08 -0600

    ✨ Add NavBar with Auth conditional rendering

--- 07-react-router-dom/src/components/Navbar.jsx
+++ 07-react-router-dom/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({ isAuth, onLogin, onLogout }) => {
   return (
     <nav className='navbar'>
       <ul>
@@ -10,7 +10,22 @@ const Navbar = () => {
         <li>
           <NavLink to='/characters'>Personajes</NavLink>
         </li>
+        {isAuth && (
+          <li>
+            <NavLink to='/secret'>Secreto</NavLink>
+          </li>
+        )}
       </ul>
+
+      <div className='auth-buttons'>
+        {isAuth
+          ? (
+            <button onClick={onLogout} className='btn btn-link'>Logout</button>
+            )
+          : (
+            <button onClick={onLogin} className='btn btn-link'>Login</button>
+            )}
+      </div>
     </nav>
   )
 }
